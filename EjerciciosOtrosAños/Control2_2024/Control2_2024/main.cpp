5f6e3a3b04dc662296ae856442ee3217a9ed8f0f
sevillaa <100518078+sevillaa@users.noreply.github.com>
2025-03-13 14:03:36 +0100

    control2 de 2024 completado

--- EjerciciosOtrosAños/Control2_2024/Control2_2024/main.cpp
+++ EjerciciosOtrosAños/Control2_2024/Control2_2024/main.cpp
@@ -10,15 +10,44 @@ using namespace std;
 #include <string>
 
 #include "lista.h"
+#include "pila.h"
+
 // INCLUIR EL RESTO DE TADS QUE SE CONSIDEREN OPORTUNOS
 
-
+bool esVocal(char c){
+    string vocales = "aAeEiIoOuU";
+    return vocales.find(c) != string::npos;
+}
 
 // Reemplaza cada secuencia de caracteres no vocales consecutivos
 // por su inversa. 'mensaje' se deber· modificar con el resultado
 // de realizar dicho proceso.
 void codifica(Lista<char> &mensaje) {
     // A IMPLEMENTAR
+    Pila<char> consonantes;
+    Lista<char>::Iterator it = mensaje.begin();
+    
+    while(it!= mensaje.end()){
+        if(!esVocal(it.elem())){
+            consonantes.apila(it.elem());
+            it = mensaje.eliminar(it);
+        }else{
+            //Caso en el que hay consonantes en la pila
+            while(!consonantes.esVacia()){
+               //tienes que insertar justo a la izquierda del iterador desapilando
+                mensaje.insertar(consonantes.cima(), it);
+                consonantes.desapila();
+            }
+
+            it.next();
+        }
+    }
+    while(!consonantes.esVacia()){
+       //tienes que insertar justo a la izquierda del iterador desapilando
+        mensaje.insertar(consonantes.cima(), it);
+        consonantes.desapila();
+    }
+
 }
 
  
